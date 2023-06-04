f6b12a8f7fdc913a3eb63ef43078f7bd69187c74
Abhishek-N <abhisheknshetty97@gmail.com>
2023-06-04 20:45:19 +0530

    add db secrets

--- env-download.sh
+++ env-download.sh
@@ -7,3 +7,15 @@ airbyte_folder="/home/ddp/airbyte"
 
 # Download .env file
 curl -o $airbyte_folder/$dot_env $base_github_url$dot_env
+
+secret=$(aws secretsmanager get-secret-value --secret-id dbsecrets --query SecretString --output text)
+secret=$(echo $secret | sed 's/\\/\\\\/g')
+
+database=$(echo $secret | jq -r '.[4].database')
+username=$(echo $secret | jq -r '.[4].username')
+password=$(echo $secret | jq -r '.[4].password')
+
+# Set the variables in your .env file
+sed -i "s/^CONFIG_DATABASE_USER=.*/CONFIG_DATABASE_USER=$username/" $airbyte_folder/$dot_env
+sed -i "s/^CONFIG_DATABASE_PASSWORD=.*/CONFIG_DATABASE_PASSWORD=$password/" $airbyte_folder/$dot_env
+sed -i "s/^CONFIG_DATABASE_URL=.*/CONFIG_DATABASE_URL=$database/" $airbyte_folder/$dot_env
