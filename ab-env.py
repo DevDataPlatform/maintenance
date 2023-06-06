c56c0f874ac2361bafaa1b412165a0ebbea798fc
Abhishek-N <abhisheknshetty97@gmail.com>
2023-06-06 12:06:26 +0530

    update os.system os to subprocess.check_output

--- ab-env.py
+++ ab-env.py
@@ -1,6 +1,6 @@
-import os
 import json
 import subprocess
+
 
 VERSION = "0.44.3"
 base_github_url = f"https://raw.githubusercontent.com/airbytehq/airbyte-platform/v{VERSION}/"
@@ -9,7 +9,7 @@ airbyte_folder = "/home/ddp/airbyte"
 
 
 # Download .env file
-os.system(f"curl -o {airbyte_folder}/{dot_env} {base_github_url}{dot_env}")
+subprocess.check_output(["curl", "-o", f"{airbyte_folder}/{dot_env}", f"{base_github_url}{dot_env}"])
 
 secret = subprocess.check_output(['aws', 'secretsmanager', 'get-secret-value', '--secret-id', 'dbsecrets', '--query', 'SecretString', '--output', 'text'])
 secret = secret.decode()
@@ -29,6 +29,6 @@ for item in secret:
 
 
 # Set the variables in your .env file
-os.system(f"sed -i '' -e 's/^CONFIG_DATABASE_USER=.*/CONFIG_DATABASE_USER={username}/' {airbyte_folder}/{dot_env}")
-os.system(f"sed -i '' -e 's/^CONFIG_DATABASE_PASSWORD=.*/CONFIG_DATABASE_PASSWORD={password}/' {airbyte_folder}/{dot_env}")
-os.system(f"sed -i '' -e 's/^CONFIG_DATABASE_URL=.*/CONFIG_DATABASE_URL={database}/' {airbyte_folder}/{dot_env}")
+subprocess.check_output(["sed", "-i", "", "-e", f"s/^CONFIG_DATABASE_USER=.*/CONFIG_DATABASE_USER={username}/", f"{airbyte_folder}/{dot_env}"])
+subprocess.check_output(["sed", "-i", "", "-e", f"s/^CONFIG_DATABASE_PASSWORD=.*/CONFIG_DATABASE_PASSWORD={password}/", f"{airbyte_folder}/{dot_env}"])
+subprocess.check_output(["sed", "-i", "", "-e", f"s/^CONFIG_DATABASE_URL=.*/CONFIG_DATABASE_URL={database}/", f"{airbyte_folder}/{dot_env}"])
